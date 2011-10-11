266c7123bbdd4f9848d258c164565dfdac89742d
James Moore <james@restphone.com>
2011-10-11 09:52:38 -0700

    Removed dead comments

--- jruby/dep_creator.rb
+++ jruby/dep_creator.rb
@@ -9,13 +9,3 @@ require 'can_opener'
 tracker = DependencyTracker.new
 tracker.populate ARGV
 puts tracker.to_json
-
-#
-#paths = tracker.calculate_class_file_paths 'com/restphone/MySampleApp' => 1
-#puts "paths are", (paths.join "\n")
-#
-#jarfile = java.util.jar.JarFile.new '/Users/james/lib/scala-2.9.1.final/lib/scala-library.jar'
-#entries = CanOpener.zip_entries_matching jarfile, paths
-#entries.each do |e|
-#  CanOpener.extract_file jarfile, e, 'scalastuff'
-#end
