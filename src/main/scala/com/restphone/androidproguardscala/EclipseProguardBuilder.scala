e91955c5a73acc2e4abacc6b7e4df3ff21de3499
James Moore <james@restphone.com>
2012-01-14 11:59:38 -0800

    Added a new Activator Added code to use the location of the plugin itself

--- src/main/scala/com/restphone/androidproguardscala/EclipseProguardBuilder.scala
+++ src/main/scala/com/restphone/androidproguardscala/EclipseProguardBuilder.scala
@@ -9,22 +9,30 @@ import org.eclipse.core.resources.ResourcesPlugin
 import scala.tools.eclipse.ScalaProject
 import org.eclipse.jdt.core.JavaCore
 import java.net.URI
+
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Path
+import org.eclipse.core.runtime.FileLocator
+import org.eclipse.core.runtime.Platform
+import org.osgi.framework.BundleContext;
+
+import java.util.ResourceBundle
 
 class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
   override def build(kind: Int, args: java.util.Map[String, String], monitor: IProgressMonitor): Array[IProject] = {
     val a = mapAsScalaMap(args)
-    com.restphone.JrubyEnvironmentSetup.addJrubyJarfile(pathForJarFile(classOf[org.jruby.Ruby]))
+    JrubyEnvironmentSetup.addJrubyJarfile(pathForJarFile(classOf[org.jruby.Ruby]))
+
     loadClassIntoJRuby(classOf[org.objectweb.asm.Type])
     loadClassIntoJRuby(classOf[proguard.Initializer])
-    
-    com.restphone.JrubyEnvironmentSetup.addToLoadPath(relativeToRoot("jruby"))
+    JrubyEnvironmentSetup.addToLoadPath(relativeToRoot("jruby"))
+    println("plugin ins asdf " + pluginDirectory)
 
     val outputJar = relativeToRoot("scala_compressed.jar")
     val cacheDirectory = relativeToRoot("proguard_cache")
     val cachedJar = relativeToRoot("proguard_cache/scala-library.CKSUM.jar")
     val proguardConfFile = relativeToRoot("proguard.conf")
-    val jr = new com.restphone.ProguardCacheRuby
+    val jr = new ProguardCacheRuby
     jr.build_dependency_files_and_final_jar(outputFoldersPathsAsStrings, proguardConfFile, outputJar, cacheDirectory, cachedJar)
     Array.empty[IProject]
   }
@@ -44,22 +52,41 @@ class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
   def rootDirectoryOfWorkspace = {
     ResourcesPlugin.getWorkspace.getRoot.getLocation.toOSString
   }
-  
+
   def pathForJarFile[T](c: Class[T]) = {
     c.getProtectionDomain.getCodeSource.getLocation.getPath
   }
-  
-  
+
   def loadClassIntoJRuby[T](c: Class[T]) = {
     val p = pathForJarFile(c)
     loadJarIntoJRuby(p)
   }
-  
+
   def loadJarIntoJRuby(path: String) = {
-    com.restphone.JrubyEnvironmentSetup.addToLoadPath(path)
+    JrubyEnvironmentSetup.addToLoadPath(path)
+  }
+
+  def pluginDirectory = {
+    val bundle = Platform.getBundle("com.restphone.androidproguardscala");
+    println("bundle is " + bundle)
+    val path = new Path("jruby");
+    val fileURL = FileLocator.find(bundle, path, null);
+    fileURL.toString
   }
 }
 
 object AndroidProguardScalaBuilder {
-  val BUILDER_ID = "AndroidProguardScala.androidProguardScala";
+  val BUILDER_ID = "com.restphone.androidproguardscala.Builder";
+}
+
+class Activator extends org.eclipse.ui.plugin.AbstractUIPlugin {
+  override def startup = {
+    println("starwerup")
+    super.startup();
+  }
+
+  override def start(context: BundleContext) {
+    println("contextaseer is " + context)
+    super.start(context);
+  }
 }
