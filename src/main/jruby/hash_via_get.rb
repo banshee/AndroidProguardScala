42ed5cd523e6cd0faab06e1dab51b6855d3a89a4
James Moore <james@restphone.com>
2012-01-21 13:10:57 -0800

    Removed unused code

--- src/main/jruby/hash_via_get.rb
+++ src/main/jruby/hash_via_get.rb
@@ -1,20 +1,6 @@
 require 'java'
 
 class HashViaGet
-  def initialize obj
-    @obj = obj
-  end
-
-  def [] key
-    case @obj
-    when Hash, HashViaGet
-      @obj[key]
-    when Java::ScalaCollectionMutable::HashMap, Java::ScalaCollectionImmutable::HashMap::HashTrieMap
-      result = @obj.get(key)
-      result.get rescue result
-    end
-  end
-
   def self.create_hash h
     result = {}
     require 'pp'
