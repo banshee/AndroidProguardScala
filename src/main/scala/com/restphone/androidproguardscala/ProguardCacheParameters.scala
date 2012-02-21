792c483307edc5990f2a42d53b53a7a0768e214e
James Moore <james@restphone.com>
2012-02-20 16:28:36 -0800

    Comments for parameters

--- src/main/scala/com/restphone/androidproguardscala/ProguardCacheParameters.scala
+++ src/main/scala/com/restphone/androidproguardscala/ProguardCacheParameters.scala
@@ -1,26 +1,17 @@
 package com.restphone.androidproguardscala
 
-//
-//case class String(path: String) {
-//  override def toString = path
-//}
-//
-//case object String {
-//  implicit def convertToString(p: String) = p.path
-//}
-
 case class ProguardCacheParameters(
   cacheDir: String,
   confDir: String,
-  workspaceDir: String,
-  projectDir: String,
-  proguardAdditionsFile: String,
-  proguardProcessedConfFile: String,
-  cachedJar: String,
-  outputJar: String,
-  scalaLibraryJar: String,
-  androidLibraryJar: String,
-  classFiles: Array[String],
-  extraLibs: Array[String],
-  proguardDefaults: String,
+  workspaceDir: String, // Provided for use in additional_libs - can be "" but not null 
+  projectDir: String, // Provided for use in additional_libs - can be "" but not null 
+  proguardAdditionsFile: String, // Path to file added to configuration - can be ""
+  proguardProcessedConfFile: String, // The final proguard configuration file
+  cachedJar: String, // Where to store cached minified jar file.  Needs to contain the string CKSUM.  (There will be many of these with different checksums)
+  outputJar: String, // Final destination for the jar file (Only one of these, this is the jar included in the final android build)
+  scalaLibraryJar: String, // path to scala-library.jar
+  androidLibraryJar: String, // path to android.jar
+  classFiles: Array[String], // directories and/or jar files; used in proguard -injar option
+  extraLibs: Array[String], // passed to proguard -libraryjars
+  proguardDefaults: String, // a string to add to the proguard conf file
   logger: ProvidesLogging)
