24fab32b6b1a751007be01fa923601d0537845d6
James Moore <james@restphone.com>
2012-11-20 09:31:16 -0800

    build changes

--- jarbuild.sh
+++ jarbuild.sh
@@ -1,11 +1,16 @@
-mkdir -p tmp
-rm -f tmp/ProjectUtil*
-cp bin/com/restphone/androidproguardscala/ProjectUtilities* tmp
-( cd bin ; jar cf /tmp/jar1.jar `find com -name \*.class` )
-rm -f /tmp/jarScalaReplaced.jar
-rm -f /tmp/jarfinal.jar
-java -jar ~/lib/jarjar.jar process lib/r1 /tmp/jar1.jar /tmp/jarScalaReplaced.jar
-java -jar ~/lib/jarjar.jar process lib/r2 /tmp/jarScalaReplaced.jar /tmp/jarfinal.jar
-( cd bin ; jar xf /tmp/jarfinal.jar )
-mv tmp/ProjectUt* bin/com/restphone/androidproguardscala/
+# Run all classfiles in bin directory through jarjar to move scala.* to com.restphone.scala.*
+tmpjar1=/tmp/jar1.jar
+tmpjar2=/tmp/jar2.jar
+( cd bin ; jar cf $tmpjar1 `find com -name \*.class` )
+java -jar ~/lib/jarjar.jar process jarjarrule.move_scala_to_com_restphone $tmpjar1 $tmpjar2
+( cd bin ; jar xf $tmpjar2 )
+
+# Run all lib files through jarjar
+mkdir -p jarjar
+for i in lib/*
+do
+  java -jar ~/lib/jarjar.jar process jarjarrule.move_scala_to_com_restphone $i jarjar/`basename $i`
+done
+
+#cleanup build artifacts
 rm -rf /tmp/plug?
