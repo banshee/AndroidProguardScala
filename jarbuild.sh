361b25125216b019e39e8f2cd59da686ff3d4a7e
James Moore <james@restphone.com>
2013-01-12 12:44:16 -0800

    use jarjar files instead of lib files for the plugin.  (jarjar files have been processed to move scala library calls to a different package)

--- jarbuild.sh
+++ jarbuild.sh
@@ -1,12 +1,14 @@
 # This is hardcoded to a single machine; that's obviouly bad.
 
+# java -jar C:/Users/james/eclipse/plugins/org.apache.ivy.eclipse.ant_2.3.0.cr2_20121105223351/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
+rm lib/*jar
+rm jarjar/*jar
 java -jar /Users/james/backupEclipseIndigo/plugins/org.apache.ivy.eclipse.ant_2.2.0.final_20100923230623/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
-java -jar C:/Users/james/eclipse/plugins/org.apache.ivy.eclipse.ant_2.3.0.cr2_20121105223351/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
 
 # Run all classfiles in bin directory through jarjar to move scala.* to com.restphone.scala.*
 tmpjar1=/tmp/jar1.jar
 tmpjar2=/tmp/jar2.jar
-( cd bin ; jar cf $tmpjar1 `find com -name /*.class` )
+( cd bin ; jar cf $tmpjar1 `find com -name \*.class` )
 java -jar ~/lib/jarjar.jar process jarjarrule.move_scala_to_com_restphone $tmpjar1 $tmpjar2
 ( cd bin ; jar xf $tmpjar2 )
 
