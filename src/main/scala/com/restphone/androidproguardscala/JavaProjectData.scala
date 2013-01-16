f8dd1c9899f5177284264d4179a01233b6d0f212
James Moore <james@restphone.com>
2013-01-15 21:51:05 -0800

    Fix for https://github.com/banshee/AndroidProguardScala/issues/22
    Preferences weren't set until you visited the preferences page

--- src/main/scala/com/restphone/androidproguardscala/JavaProjectData.scala
+++ src/main/scala/com/restphone/androidproguardscala/JavaProjectData.scala
@@ -12,41 +12,72 @@ import scala.PartialFunction._
 import scalaz._
 import Scalaz._
 import org.eclipse.core.internal.runtime.Log
+import org.eclipse.jdt.internal.core.ClasspathEntry
 
-class JavaProjectData( project: IJavaProject ) {
+class JavaProjectData(project: IJavaProject) {
   def getProject = project.getProject
 
   def classpathEntries: Array[ClasspathEntryData] =
+    classpathData map { _.data }
+
+  case class ClasspathData(entry: IClasspathEntry, data: ClasspathEntryData)
+
+  private def classpathData =
     for {
-      classpathentry <- project.getResolvedClasspath( true ) if ( ( classpathentry.getContentKind ^ IPackageFragmentRoot.K_SOURCE ) != 0 )
-      prefname <- ClasspathEntryData.convertPathToPreferenceName( classpathentry.getPath.toOSString )
-    } yield ClasspathEntryData( prefname, classpathentry.getPath.toOSString )
+      classpathentry <- project.getResolvedClasspath(true) if ((classpathentry.getContentKind ^ IPackageFragmentRoot.K_SOURCE) != 0)
+      prefname <- ClasspathEntryData.convertPathToPreferenceName(classpathentry.getPath.toOSString)
+    } yield ClasspathData(classpathentry, ClasspathEntryData(prefname, classpathentry.getPath.toOSString))
 
   def outputDirectories = {
-    val cpes = project.getResolvedClasspath( true ) filter { _.getEntryKind == IClasspathEntry.CPE_SOURCE } filter { _.getContentKind == IPackageFragmentRoot.K_SOURCE }
-    val specificOutputLocations = cpes flatMap { c => Option( c.getOutputLocation ) }
-    val optionalDirectories = ( project.getOutputLocation :: specificOutputLocations.toList ).toSet map convertPathToFilesystemPath
+    val cpes = project.getResolvedClasspath(true) filter { _.getEntryKind == IClasspathEntry.CPE_SOURCE } filter { _.getContentKind == IPackageFragmentRoot.K_SOURCE }
+    val specificOutputLocations = cpes flatMap { c => Option(c.getOutputLocation) }
+    val optionalDirectories = (project.getOutputLocation :: specificOutputLocations.toList).toSet map convertPathToFilesystemPath
     optionalDirectories.flatten
   }
 
-  lazy val preferences = {
-    val projectScope: IScopeContext = new ProjectScope( getProject )
-    new ScopedPreferenceStore( projectScope, "com.restphone.androidproguardscala" );
+  val preferences = {
+    val projectScope: IScopeContext = new ProjectScope(getProject)
+    val store = new ScopedPreferenceStore(projectScope, "com.restphone.androidproguardscala");
+    for {
+      ClasspathData(entry, data) <- classpathData
+      defaultValue = defaultValueForClasspathEntry(entry)
+    } yield {
+      store.setDefault(data.fieldName, defaultValue.asString)
+    }
+    store
   }
 
-  def inputJars = jarsOfType( InputJar )
-  def libraryJars = jarsOfType( LibraryJar )
-
-  private def jarsOfType( t: ClasspathEntryType ) = {
-    classpathEntries filter { stateForClasspathEntryData( _ ) == t } toList
+  def defaultValueForClasspathEntry(c: IClasspathEntry): ClasspathEntryType = {
+    val MatchesAndroidSdk = ".*android-sdk.*".r
+    val AndroidSupport = """android-support-v\d+.jar""".r
+    val Scalaz = """scalaz-.*jar""".r
+    val Akka = """akka-.*jar""".r
+    val (a, b) = (c.getPath.toOSString, c.getPath.lastSegment)
+    val result = (a, b) match {
+      case (_, ("scala-library.jar" | "scala-swing.jar" | "scala-actors.jar" | "scala-reject.jar")) => InputJar
+      case (_, Scalaz()) => InputJar
+      case (_, Akka()) => InputJar
+      case (_, "android.jar") => LibraryJar
+      case (_, MatchesAndroidSdk()) => LibraryJar
+      case (_, AndroidSupport()) => LibraryJar
+      case _ => IgnoredJar
+    }
+    result
   }
 
-  private def stateForClasspathEntryData( c: ClasspathEntryData ): ClasspathEntryType = {
-    val pref =  preferences.getString( c.fieldName )
-    ClasspathEntryType.convertStringToClasspathEntryType(pref ) getOrElse (IgnoredJar)
+  def inputJars = jarsOfType(InputJar)
+  def libraryJars = jarsOfType(LibraryJar)
+
+  private def jarsOfType(t: ClasspathEntryType) = {
+    classpathEntries filter { stateForClasspathEntryData(_) == t } toList
   }
 
-  def convertPathToFilesystemPath( p: IPath ) = {
+  private def stateForClasspathEntryData(c: ClasspathEntryData): ClasspathEntryType = {
+    val pref = preferences.getString(c.fieldName)
+    ClasspathEntryType.convertStringToClasspathEntryType(pref) getOrElse (IgnoredJar)
+  }
+
+  def convertPathToFilesystemPath(p: IPath) = {
     for {
       root <- Option(ResourcesPlugin.getWorkspace().getRoot())
       member <- Option(root.findMember(p))
