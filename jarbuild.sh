1a053697f7c31177bdc07221c221ff99f91b7f82
James Moore <james@restphone.com>
2013-01-27 13:24:49 -0800

    Don't always run the ivy fetch

--- jarbuild.sh
+++ jarbuild.sh
@@ -5,7 +5,7 @@ jarjar=lib/jarjar-jar-jarjar-1.3.jar
 # java -jar C:/Users/james/eclipse/plugins/org.apache.ivy.eclipse.ant_2.3.0.cr2_20121105223351/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
 rm lib/*jar
 rm jarjar/*jar
-java -jar /Users/james/backupEclipseIndigo/plugins/org.apache.ivy.eclipse.ant_2.2.0.final_20100923230623/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
+# java -jar /Users/james/backupEclipseIndigo/plugins/org.apache.ivy.eclipse.ant_2.2.0.final_20100923230623/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
 
 # Run all classfiles in bin directory through jarjar to move scala.* to com.restphone.scala.*
 tmpjar1=/tmp/jar1.jar
