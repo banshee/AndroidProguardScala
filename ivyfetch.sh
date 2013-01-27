6d3ce6497c2a141d461793a1b5b6c65e4ac3eee7
James Moore <james@restphone.com>
2013-01-27 14:35:56 -0800

    Separate ivy fetch and jarbuild

---
+++ ivyfetch.sh
@@ -0,0 +1,9 @@
+jarjar=lib/jarjar-jar-jarjar-1.3.jar
+
+# java -jar C:/Users/james/eclipse/plugins/org.apache.ivy.eclipse.ant_2.3.0.cr2_20121105223351/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
+rm lib/*jar
+rm jarjar/*jar
+java -jar /Users/james/backupEclipseIndigo/plugins/org.apache.ivy.eclipse.ant_2.2.0.final_20100923230623/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]"
+rm lib/*source*
+rm lib/*javadoc* 
+ls -l lib
--- jarbuild.sh
+++ jarbuild.sh
@@ -2,17 +2,13 @@
 
 jarjar=lib/jarjar-jar-jarjar-1.3.jar
 
-# java -jar C:/Users/james/eclipse/plugins/org.apache.ivy.eclipse.ant_2.3.0.cr2_20121105223351/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
-rm lib/*jar
-rm jarjar/*jar
-# java -jar /Users/james/backupEclipseIndigo/plugins/org.apache.ivy.eclipse.ant_2.2.0.final_20100923230623/ivy.jar -ivy ivy.xml  -settings ../ivysettings.xml -retrieve "lib/[module]-[type]-[artifact]-[revision].[ext]" ; rm lib/*source* ; rm lib/*javadoc* ; ls -l lib
-
 # Run all classfiles in bin directory through jarjar to move scala.* to com.restphone.scala.*
 tmpjar1=/tmp/jar1.jar
 tmpjar2=/tmp/jar2.jar
 ( cd bin ; jar cf $tmpjar1 `find com -name \*.class` )
 java -jar $jarjar process jarjarrule.move_scala_to_com_restphone $tmpjar1 $tmpjar2
-( cd bin ; jar xf $tmpjar2 )
+mkdir -p jarjarbin
+( cd jarjarbin ; rm -rf com org ; jar xf $tmpjar2 )
 
 # Run all lib files through jarjar
 mkdir -p jarjar
