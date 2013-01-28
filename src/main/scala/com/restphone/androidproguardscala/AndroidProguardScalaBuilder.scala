5f6eb9b1111eec05e71058db5f8454daaf4944f5
James Moore <james@restphone.com>
2013-01-27 17:22:15 -0800

    Allow reading defaults from jarfile

--- src/main/scala/com/restphone/androidproguardscala/AndroidProguardScalaBuilder.scala
+++ src/main/scala/com/restphone/androidproguardscala/AndroidProguardScalaBuilder.scala
@@ -2,9 +2,7 @@ package com.restphone.androidproguardscala
 
 import java.io.File
 import java.io.ObjectStreamException
-
 import scala.Array.canBuildFrom
-
 import org.eclipse.core.resources.IProject
 import org.eclipse.core.resources.IResource
 import org.eclipse.core.resources.IResourceDelta
@@ -20,7 +18,6 @@ import org.eclipse.core.runtime.Platform
 import org.eclipse.core.runtime.Status
 import org.eclipse.jdt.core.IClasspathEntry
 import org.eclipse.jdt.core.JavaCore
-
 import com.google.common.io.Files
 import com.restphone.jartender.BuiltLibrary
 import com.restphone.jartender.CacheSystem
@@ -33,30 +30,30 @@ import com.restphone.jartender.FileFailureValidation.validatedFile
 import com.restphone.jartender.JartenderCacheParameters
 import com.restphone.jartender.RichFile
 import com.restphone.jartender.SerializableUtilities
-
 import scalaz.Scalaz._
+import java.net.URL
 
 class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
   import RichPath._
 
-  def buildPatternMatch[T]( fn: T => Boolean ) = new Object {
-    def unapply[U <% T]( x: U ) = if ( fn( x ) ) some( x ) else none
+  def buildPatternMatch[ T ]( fn : T => Boolean ) = new Object {
+    def unapply[ U <% T ]( x : U ) = if ( fn( x ) ) some( x ) else none
   }
 
-  override def build( kind: Int, args: java.util.Map[String, String], monitor: IProgressMonitor ): Array[IProject] = {
+  override def build( kind : Int, args : java.util.Map[ String, String ], monitor : IProgressMonitor ) : Array[ IProject ] = {
     val buildRequired = {
-      val pathIsBuildArtifact = buildPatternMatch[IPath]( _.lastSegment.startsWith( "proguard_" ) )
-      def buildArtifactsRequireRebuild( xs: List[IPath] ): Boolean = {
+      val pathIsBuildArtifact = buildPatternMatch[ IPath ]( _.lastSegment.startsWith( "proguard_" ) )
+      def buildArtifactsRequireRebuild( xs : List[ IPath ] ) : Boolean = {
         xs match {
           case pathIsBuildArtifact( h ) :: Nil => false
-          case pathIsBuildArtifact( h ) :: t => buildArtifactsRequireRebuild( t )
-          case _ => true
+          case pathIsBuildArtifact( h ) :: t   => buildArtifactsRequireRebuild( t )
+          case _                               => true
         }
       }
 
-      val affected_paths: List[IPath] = getDelta( getProject ) match {
-        case x: IResourceDelta => ( x.getAffectedChildren map { _.getFullPath } ).toList
-        case null => List.empty
+      val affected_paths : List[ IPath ] = getDelta( getProject ) match {
+        case x : IResourceDelta => ( x.getAffectedChildren map { _.getFullPath } ).toList
+        case null               => List.empty
       }
 
       buildArtifactsRequireRebuild( affected_paths )
@@ -65,7 +62,12 @@ class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
     if ( buildRequired ) {
       val proguardDefaults = {
         val pathToDefaultsFile = pluginDirectory.get / "proguard_cache_conf" / "proguard_defaults.conf"
-        RichFile.slurp( pathToDefaultsFile.toFile )
+        val url = new URL( "platform:/plugin/com.restphone.androidproguardscala/proguard_cache_conf/proguard_defaults.conf" );
+        val inputStream = url.openConnection().getInputStream();
+        val s = scala.io.Source.fromInputStream( inputStream )
+        val result = s.getLines.mkString( "\n" )
+        s.close()
+        result
       }
 
       Seq( cacheDir, confDir, libDirectory ) foreach RichPath.ensureDirExists
@@ -78,7 +80,7 @@ class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
       val outputJar = rootDirectoryOfProject / "libs" / minifiedScalaLibraryName
       val existingOutputFolders = projectdata.outputDirectories
 
-      implicit def convertIPathToString( p: IPath ): String = p.toString
+      implicit def convertIPathToString( p : IPath ) : String = p.toString
 
       val jartenderParams = JartenderCacheParameters(
         cacheDir = cacheDir,
@@ -105,9 +107,9 @@ class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
         val lnx = bytesForCache.length
         Files.write( bytesForCache, cacheFile )
         cacheEntry match {
-          case _: BuiltLibrary =>
+          case _ : BuiltLibrary =>
             Iterable( outputJar, confDir, cacheDir ) foreach tellEclipsePathNeedsToBeRefreshed
-          case _: ExistingLibrary =>
+          case _ : ExistingLibrary =>
         }
         cacheEntry
       }
@@ -120,17 +122,17 @@ class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
     Array.empty
   }
 
-  def readCacheSystemFromFile( f: File ) = {
-    val deserializeExceptions = classOf[ClassNotFoundException] :: classOf[ObjectStreamException] :: classOf[ClassCastException] :: stdExceptions
+  def readCacheSystemFromFile( f : File ) = {
+    val deserializeExceptions = classOf[ ClassNotFoundException ] :: classOf[ ObjectStreamException ] :: classOf[ ClassCastException ] :: stdExceptions
     implicit val safeDeserialize = convertExceptionToValidation( deserializeExceptions )_
 
-    def bytesToCacheSystem( bytes: Array[Byte] ): FailureValidation[CacheSystem] =
+    def bytesToCacheSystem( bytes : Array[ Byte ] ) : FailureValidation[ CacheSystem ] =
       for {
         x <- safeDeserialize( "deserialize CacheSystem", none ) {
-          val obj: Option[CacheSystem] = SerializableUtilities.byteArrayToObject( bytes )
+          val obj : Option[ CacheSystem ] = SerializableUtilities.byteArrayToObject( bytes )
           obj match {
             case Some( v ) => v.success
-            case None => FailureWithoutException( "failed to get a cache system object from bytes" ).failureNel
+            case None      => FailureWithoutException( "failed to get a cache system object from bytes" ).failureNel
           }
         }
       } yield x
@@ -142,15 +144,9 @@ class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
     } yield result
   }
 
-  def tellEclipsePathNeedsToBeRefreshed( p: IPath ) = {
+  def tellEclipsePathNeedsToBeRefreshed( p : IPath ) = {
     getProject.getFile( p ).refreshLocal( IResource.DEPTH_INFINITE, null )
   }
-
-  // We can't create the cacheController until the plugin is started.  That happens
-  // after object initialization, so it's a lazy.
-  lazy val cacheController = ProguardCacheController.buildCacheController( pluginDirectory.toString )
-
-  override def clean( monitor: IProgressMonitor ): Unit = cacheController.clean_cache( cacheDir.toString )
 
   def rootDirectoryOfProject = convertResourceToFilesystemLocation( getProject )
   def cacheDir = rootDirectoryOfProject / "proguard_cache"
@@ -158,27 +154,27 @@ class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
   def libDirectory = rootDirectoryOfProject / "libs"
   //  def scalaProject = scala.tools.eclipse.ScalaProject(getProject)
 
-  def isCpeLibrary( x: IClasspathEntry ) = x.getEntryKind == IClasspathEntry.CPE_LIBRARY
-  def isMinifiedLibraryName( s: String ) = s == minifiedScalaLibraryName
+  def isCpeLibrary( x : IClasspathEntry ) = x.getEntryKind == IClasspathEntry.CPE_LIBRARY
+  def isMinifiedLibraryName( s : String ) = s == minifiedScalaLibraryName
 
-  def convertResourceToFilesystemLocation( resource: IResource ) = new Path( resource.getLocationURI.getPath )
+  def convertResourceToFilesystemLocation( resource : IResource ) = new Path( resource.getLocationURI.getPath )
 
   def logger() = new ProvidesLogging {
-    def logMsg( msg: String ) = AndroidProguardScalaBuilder.this.logMsg( msg )
-    def logError( msg: String ) = AndroidProguardScalaBuilder.this.logMsg( msg, IStatus.ERROR )
+    def logMsg( msg : String ) = AndroidProguardScalaBuilder.this.logMsg( msg )
+    def logError( msg : String ) = AndroidProguardScalaBuilder.this.logMsg( msg, IStatus.ERROR )
   }
 
-  private val lastSegmentIsString = ( s: String ) => ( p: IPath ) => p.lastSegment.equals( s )
+  private val lastSegmentIsString = ( s : String ) => ( p : IPath ) => p.lastSegment.equals( s )
   val lastSegmentIsScalaLibrary = lastSegmentIsString( "scala-library.jar" )
   val lastSegmentIsAndroidLibrary = lastSegmentIsString( "android.jar" )
-  val fileExists = ( p: IPath ) => p.toFile.exists
+  val fileExists = ( p : IPath ) => p.toFile.exists
 
   lazy val javaProject = JavaCore.create( getProject )
 
-  def objToString[T]( x: T ) = x.toString
+  def objToString[ T ]( x : T ) = x.toString
 
-  def getWorkspaceRoot: IWorkspaceRoot = ResourcesPlugin.getWorkspace.getRoot
-  def rootDirectoryOfWorkspace: IPath = getWorkspaceRoot.getLocation
+  def getWorkspaceRoot : IWorkspaceRoot = ResourcesPlugin.getWorkspace.getRoot
+  def rootDirectoryOfWorkspace : IPath = getWorkspaceRoot.getLocation
 
   val platformBundle = Platform.getBundle( "com.restphone.androidproguardscala" );
 
@@ -191,7 +187,7 @@ class AndroidProguardScalaBuilder extends IncrementalProjectBuilder {
       f = new Path( filenameUrl.getFile )
     } yield f
 
-  def logMsg( msg: String, status: Integer = IStatus.INFO ) = {
+  def logMsg( msg : String, status : Integer = IStatus.INFO ) = {
     val log = Platform.getLog( platformBundle );
     val s = new Status( status, pluginId, msg )
     log.log( s )
@@ -206,11 +202,11 @@ object AndroidProguardScalaBuilder {
   val BUILDER_ID = "com.restphone.androidproguardscala.Builder";
 }
 
-class RichPath( p: IPath ) {
-  def /( that: String ) = p.append( that )
+class RichPath( p : IPath ) {
+  def /( that : String ) = p.append( that )
 }
 
 object RichPath {
-  implicit def toRichPath( p: IPath ): RichPath = new RichPath( p )
-  def ensureDirExists( p: IPath ) = RichFile.ensureDirExists( p.toFile )
+  implicit def toRichPath( p : IPath ) : RichPath = new RichPath( p )
+  def ensureDirExists( p : IPath ) = RichFile.ensureDirExists( p.toFile )
 }
