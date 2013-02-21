070252157a1f6933f766f3b46be4c758f2efbf6a
James Moore <james@restphone.com>
2013-02-21 00:38:24 -0800

    Adding build script from scalatest

--- com.restphone.androidproguardscala.plugin/ecosystem-build.sh
+++ com.restphone.androidproguardscala.plugin/ecosystem-build.sh
@@ -46,19 +46,19 @@ rm -rf ${TARGET_DIR}
 #build scala-ide-master-scala-2.9 http://download.scala-ide.org/ecosystem/e38/scala29/dev/site/ 2.9.3-scala-ide-m2 e38-scala29-dev http://download.eclipse.org/releases/juno/
 #build scala-ide-master-scala-trunk http://download.scala-ide.org/ecosystem/e37/scala29/dev/site/ 2.10.0-RC1 e37-scala210-dev http://download.eclipse.org/releases/indigo/
 #build scala-ide-master-scala-trunk http://download.scala-ide.org/ecosystem/e38/scala29/dev/site/ 2.10.0-RC1 e38-scala210-dev http://download.eclipse.org/releases/juno/
-build scala-ide-2.1-m3 http://download.scala-ide.org/ecosystem/next/e37/scala29/dev/base/ 2.9.3-RC2 e37-scala29-m3 http://download.eclipse.org/releases/indigo/
-build scala-ide-2.1-m3 http://download.scala-ide.org/ecosystem/next/e38/scala29/dev/base/ 2.9.3-RC2 e38-scala29-m3 http://download.eclipse.org/releases/juno/
+# build scala-ide-2.1-m3 http://download.scala-ide.org/ecosystem/next/e37/scala29/dev/base/ 2.9.3-RC2 e37-scala29-m3 http://download.eclipse.org/releases/indigo/
+# build scala-ide-2.1-m3 http://download.scala-ide.org/ecosystem/next/e38/scala29/dev/base/ 2.9.3-RC2 e38-scala29-m3 http://download.eclipse.org/releases/juno/
 build scala-ide-2.1-m3-2_10 http://download.scala-ide.org/sdk/next/e37/scala210/dev/base/ 2.10.0 e37-scala210-m3 http://download.eclipse.org/releases/indigo/
-build scala-ide-2.1-m3-2_10 http://download.scala-ide.org/sdk/next/e38/scala210/dev/base/ 2.10.0 e38-scala210-m3 http://download.eclipse.org/releases/juno/
-build scala-ide-2.0.2 http://download.scala-ide.org/sdk/e37/scala29/stable/site/ 2.9.2 e37-scala29-2.0.2 http://download.eclipse.org/releases/indigo/
+# build scala-ide-2.1-m3-2_10 http://download.scala-ide.org/sdk/next/e38/scala210/dev/base/ 2.10.0 e38-scala210-m3 http://download.eclipse.org/releases/juno/
+# build scala-ide-2.0.2 http://download.scala-ide.org/sdk/e37/scala29/stable/site/ 2.9.2 e37-scala29-2.0.2 http://download.eclipse.org/releases/indigo/
 
 cd ${MERGE_TOOL_DIR}
 #mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala29-dev package
 #mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e38-scala29-dev package
 #mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala210-dev package
 #mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e38-scala210-dev package
-mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala29-m3 package
-mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e38-scala29-m3 package
-mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala210-m3 package
-mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e38-scala210-m3 package
-mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala29-2.0.2 package
+# mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala29-m3 package
+# mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e38-scala29-m3 package
+# mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala210-m3 package
+# mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e38-scala210-m3 package
+# mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala29-2.0.2 package
