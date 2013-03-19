8e8fda51d898dc06fc16dadcf63339605a42e8e1
James Moore <james@restphone.com>
2013-03-18 23:07:02 -0700

    build fixes

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
-rm -rf ~/.m2/repository/com/restphone/ /Users/jamesmoore/workspace/AndroidProguardScala/com.restphone.androidproguardscala.plugin/lib/* ; out mvn install
+cd /Users/jamesmoore/workspace/AndroidProguardScala
+rm -rf ~/.m2/repository/com/restphone/ /Users/jamesmoore/workspace/AndroidProguardScala/com.restphone.androidproguardscala.plugin/lib/*
+mvn install
