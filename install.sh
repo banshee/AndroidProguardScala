162bf635e77513b361860b3f52e5701139c38d94
James Moore <james@restphone.com>
2013-08-11 22:55:14 -0700

    modified install script

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 project_dir=~/workspace/AndroidProguardScala
 rm -rf ~/.m2/repository/com/restphone/ ${project_dir}/AndroidProguardScala/com.restphone.androidproguardscala.*/lib/*
-( cd ${project_dir}/com.restphone.androidproguardscala.plugin && MAVEN_OPTS="-Xmx5120m -XX:MaxPermSize=1208M" mvn clean install )
-MAVEN_OPTS="-Xmx5120m -XX:MaxPermSize=1208M" mvn clean install
+( cd ${project_dir}/com.restphone.androidproguardscala.parent && MAVEN_OPTS="-Xmx5120m -XX:MaxPermSize=1208M" mvn clean install )
