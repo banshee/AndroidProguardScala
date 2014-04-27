52a977592b4d0bde43b7abbbb4206cf0bd763479
James Moore <james@restphone.com>
2014-04-27 08:19:22 -0700

    v51

--- changeVersion.sh
+++ changeVersion.sh
@@ -1,3 +1,3 @@
 cd com.restphone.androidproguardscala.parent
-# mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=0.0.$1
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=0.0.$1-SNAPSHOT
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=0.0.$1
+# mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=0.0.$1-SNAPSHOT
